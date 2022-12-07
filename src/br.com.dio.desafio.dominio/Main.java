66fb2603cf52366b005467a6ff7652f6761accbf
tavazz <gabriel.tavas10@gmail.com>
2022-12-06 23:31:24 -0300

    abstracao

---
+++ src/br.com.dio.desafio.dominio/Main.java
@@ -0,0 +1,23 @@
+package br.com.dio.desafio.dominio;
+
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Java Begginners");
+        
+
+        System.out.println(curso1);
+
+
+
+
+    }
+    
+}
+
+
